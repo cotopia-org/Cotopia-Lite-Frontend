96518735f15891b8dcc3a4ecc526c32c93f3fe68
mahdirasti2 <mahdirasti.dev@gmail.com>
2024-08-04 08:07:03 +0300

    add my latest changes in control videos

--- src/components/shared/room/check-permissions/video/index.tsx
+++ src/components/shared/room/check-permissions/video/index.tsx
@@ -1,47 +1,84 @@
-import CotopiaButton from "@/components/shared-ui/c-button";
 import FullLoading from "@/components/shared/full-loading";
 import useLoading from "@/hooks/use-loading";
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import AllowVideoBtn from "./allow-video-btn";
 import { useRoomContext } from "../../room-context";
+import PermissionControls from "../controls";
+
+const VideoContext = createContext<{
+  videoStream: MediaStream | null;
+  getPermissions: () => void;
+  stopVideoStream: () => void;
+}>({
+  videoStream: null,
+  getPermissions: () => {},
+  stopVideoStream: () => {},
+});
+
+export const useVideoContext = () => useContext(VideoContext);
 
 export default function Video() {
-  const { videoState } = useRoomContext();
-
+  const { videoState, changePermissionState } = useRoomContext();
   const videoRef = useRef<HTMLVideoElement>(null);
   const [videoPermission, setVideoPermission] = useState<boolean>(false);
   const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
-
   const { startLoading, stopLoading, isLoading } = useLoading();
 
-  useEffect(() => {
+  const getPermissions = async () => {
     startLoading();
-    const getPermissions = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        setVideoPermission(true);
-        setVideoStream(stream);
-        stopLoading();
-      } catch (err) {
-        stopLoading();
-        if (err instanceof DOMException) {
-          if (err.name === "NotAllowedError") {
-            setVideoPermission(false);
-          }
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      setVideoPermission(true);
+      console.log("stream", stream);
+      setVideoStream(stream);
+      changePermissionState("video", true);
+    } catch (err) {
+      if (err instanceof DOMException) {
+        if (err.name === "NotAllowedError") {
+          setVideoPermission(false);
         }
       }
-    };
+    } finally {
+      stopLoading();
+    }
+  };
 
-    getPermissions();
-  }, []);
+  const stopVideoStream = () => {
+    if (videoStream) {
+      videoStream.getTracks().forEach((track) => track.stop());
+      setVideoStream(null);
+      setVideoPermission(false);
+      changePermissionState("video", false);
+      console.log("Video stream stopped");
+    } else {
+      console.log("No video stream to stop");
+    }
+  };
+
+  useEffect(() => {
+    if (videoState === true) {
+      getPermissions();
+    } else {
+      stopVideoStream();
+    }
+  }, [videoState]);
 
   useEffect(() => {
     if (videoRef.current && videoStream) {
       videoRef.current.srcObject = videoStream;
+      videoRef.current.play();
+    } else if (videoRef.current) {
+      videoRef.current.srcObject = null;
     }
-  }, [videoStream, videoState]);
+  }, [videoStream]);
 
   let content = (
     <>
@@ -53,7 +90,7 @@ export default function Video() {
     </>
   );
 
-  if (videoPermission === false)
+  if (videoPermission === false && videoState === true)
     content = (
       <div className='flex flex-col gap-y-2 items-center justify-center'>
         <strong className='text-xl'>
@@ -73,10 +110,15 @@ export default function Video() {
   if (isLoading) content = <FullLoading />;
 
   return (
-    <div>
-      <div className='w-full relative min-h-[300px] flex flex-col items-center justify-center bg-black/10  rounded-lg overflow-hidden'>
+    <VideoContext.Provider
+      value={{ videoStream, getPermissions, stopVideoStream }}
+    >
+      <div className='w-full relative min-h-[300px] flex flex-col items-center justify-center bg-black/10 rounded-lg overflow-hidden'>
         {content}
+        <div className='absolute bottom-0'>
+          <PermissionControls />
+        </div>
       </div>
-    </div>
+    </VideoContext.Provider>
   );
 }
