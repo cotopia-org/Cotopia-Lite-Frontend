43b0efb1f6d674d980c003e9a221f4418cf60a64
mahdirasti2 <mahdi.r@zemtrix.com>
2024-08-11 13:06:37 +0300

    Join again user in room

--- src/components/shared/room/connection-status/disconnected.tsx
+++ src/components/shared/room/connection-status/disconnected.tsx
@@ -3,27 +3,32 @@
 import NotFound from "../../layouts/not-found";
 import { RotateCcw, Unlink } from "lucide-react";
 import CotopiaButton from "@/components/shared-ui/c-button";
-import useKeyPress from "@/hooks/use-key-press";
 import { __VARS } from "@/app/const/vars";
 import { useRoomContext as CotopiaRoomContext } from "../room-context";
 import { useRoomContext } from "@livekit/components-react";
 import { useEffect } from "react";
 import socket from "@/lib/socket";
 import { useProfile } from "@/app/(pages)/(protected)/protected-wrapper";
+import axiosInstance, { FetchDataType } from "@/lib/axios";
+import { WorkspaceRoomJoinType } from "@/types/room";
 
 export default function Disconnected() {
   const { token: userToken } = useProfile();
 
   const room = useRoomContext();
 
-  const { livekit_token } = CotopiaRoomContext();
+  const { livekit_token, room_id } = CotopiaRoomContext();
 
-  const onReload = () => {
+  const onReload = async () => {
     if (!livekit_token) return;
     if (!__VARS.serverUrl) return;
 
     room.connect(__VARS.serverUrl, livekit_token);
     if (userToken) socket.connect(__VARS.socketUrl, userToken);
+
+    await axiosInstance.get<FetchDataType<WorkspaceRoomJoinType>>(
+      `/rooms/${room_id}/join`
+    );
   };
 
   useEffect(() => {
