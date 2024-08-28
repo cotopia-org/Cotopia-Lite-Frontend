f32cb9ff5a8cacfc7e88269fc2e9ab08aad6dcc9
mahdirasti2 <mahdi.r@zemtrix.com>
2024-08-28 10:13:15 +0300

    Fix direct problem for user

--- src/components/shared/room/settings/chat/direct/users/card/index.tsx
+++ src/components/shared/room/settings/chat/direct/users/card/index.tsx
@@ -1,38 +1,40 @@
-"use client"
-import CotopiaAvatar from "@/components/shared-ui/c-avatar"
-import { UserMinimalType } from "@/types/user"
-import Username from "./username"
-import Message from "./message"
+"use client";
+import CotopiaAvatar from "@/components/shared-ui/c-avatar";
+import { UserMinimalType } from "@/types/user";
+import Username from "./username";
+import Message from "./message";
 
-import { ChatItemType } from "@/types/chat"
+import { ChatItemType } from "@/types/chat";
 
 type Props = {
-  user: UserMinimalType
-  latestMessage?: ChatItemType
-  onClick: () => void
-}
+  user: UserMinimalType;
+  latestMessage?: ChatItemType;
+  onClick: () => void;
+};
 export default function UserCard({ user, latestMessage, onClick }: Props) {
-  let msgUnseen = latestMessage?.seen === false
-  let isMineMsg = user.id !== latestMessage?.user.id
+  if (!user) return;
 
-  let isMsgUnseen = msgUnseen && !isMineMsg
+  let msgUnseen = latestMessage?.seen === false;
+  let isMineMsg = user?.id !== latestMessage?.user.id;
 
-  let userNameClss = ""
-  let lastMsgClss = ""
+  let isMsgUnseen = msgUnseen && !isMineMsg;
+
+  let userNameClss = "";
+  let lastMsgClss = "";
 
   if (isMsgUnseen) {
-    userNameClss += " !text-black"
-    lastMsgClss += " !font-semibold !text-black/[0.78]"
+    userNameClss += " !text-black";
+    lastMsgClss += " !font-semibold !text-black/[0.78]";
   }
 
   return (
     <div
-      className="flex flex-row items-center gap-x-4 cursor-pointer"
+      className='flex flex-row items-center gap-x-4 cursor-pointer'
       onClick={onClick}
     >
-      <div className="relative">
+      <div className='relative'>
         {isMsgUnseen ? (
-          <div className="rounded-full w-[10px] h-[10px] z-[2] absolute bg-red-500 top-0 right-0"></div>
+          <div className='rounded-full w-[10px] h-[10px] z-[2] absolute bg-red-500 top-0 right-0'></div>
         ) : null}
         <CotopiaAvatar
           src={user?.avatar?.url ?? undefined}
@@ -40,12 +42,12 @@ export default function UserCard({ user, latestMessage, onClick }: Props) {
         />
       </div>
 
-      <div className="flex flex-col">
+      <div className='flex flex-col'>
         <Username className={userNameClss} username={user?.username} />
         {!!latestMessage && (
           <Message className={lastMsgClss} message={latestMessage.text} />
         )}
       </div>
     </div>
-  )
+  );
 }
