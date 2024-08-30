970489694cc5d144a9280ab68807596bd2405558
mahdirasti2 <mahdi.r@zemtrix.com>
2024-08-30 14:12:15 +0300

    also check room_id

--- src/components/shared/room/tools/top-right/time-tracking/details/index.tsx
+++ src/components/shared/room/tools/top-right/time-tracking/details/index.tsx
@@ -48,7 +48,8 @@ export default function TimeTrackingDetails() {
                 <span className='text-xs'>{item.user?.name ?? "-"}</span>
               </div>
               <div>
-                {item?.user?.status === "online" ? (
+                {item?.user?.status === "online" &&
+                item.user?.room_id !== null ? (
                   <Timer initialSeconds={item.sum_minutes * 60}>
                     {(time) => <strong className='text-xs'>{time}</strong>}
                   </Timer>
