2d16f8c8c81946229b9167040ea5537ed006a190
mahdirasti2 <mahdi.r@zemtrix.com>
2024-09-01 11:43:41 +0300

    remove seen emit message

--- src/components/shared/chat-box/chat-item/message/index.tsx
+++ src/components/shared/chat-box/chat-item/message/index.tsx
@@ -1,52 +1,52 @@
-import { useSocket } from "@/app/(pages)/(protected)/protected-wrapper"
-import { useChatRoomCtx } from "@/context/chat-room-context"
-import { ChatItemType } from "@/types/chat"
-import { useEffect, useRef, useState } from "react"
-import Linkify from "react-linkify"
+import { useSocket } from "@/app/(pages)/(protected)/protected-wrapper";
+import { useChatRoomCtx } from "@/context/chat-room-context";
+import { ChatItemType } from "@/types/chat";
+import { useEffect, useRef, useState } from "react";
+import Linkify from "react-linkify";
 
 type Props = {
-  isMine: boolean
-  item: ChatItemType
-}
+  isMine: boolean;
+  item: ChatItemType;
+};
 export default function Message({ item, isMine }: Props) {
-  const divRef = useRef<HTMLDivElement>(null)
+  const divRef = useRef<HTMLDivElement>(null);
 
-  const { roomId } = useChatRoomCtx()
+  const { roomId } = useChatRoomCtx();
 
-  const socket = useSocket()
-  const [isVisible, setIsVisible] = useState(false)
+  const socket = useSocket();
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        setIsVisible(entry.isIntersecting)
+        setIsVisible(entry.isIntersecting);
       },
       { threshold: 0.5 } // Trigger when 50% of the element is visible
-    )
+    );
 
     if (divRef.current) {
-      observer.observe(divRef.current)
+      observer.observe(divRef.current);
     }
 
     return () => {
       if (divRef.current) {
-        observer.unobserve(divRef.current)
+        observer.unobserve(divRef.current);
       }
-    }
-  }, [])
+    };
+  }, []);
 
-  useEffect(() => {
-    if (isVisible && item?.seen === false && !isMine) {
-      if (socket) {
-        socket.emit("seenMessage", { room_id: roomId, message: item })
-      }
-    }
-  }, [item, isVisible, isMine, socket])
+  // useEffect(() => {
+  //   if (isVisible && item?.seen === false && !isMine) {
+  //     if (socket) {
+  //       socket.emit("seenMessage", { room_id: roomId, message: item })
+  //     }
+  //   }
+  // }, [item, isVisible, isMine, socket])
 
   return (
     <div
-      className="text-wrap mb-3 w-full"
-      dir="auto"
+      className='text-wrap mb-3 w-full'
+      dir='auto'
       style={{ overflowWrap: "break-word" }}
       ref={divRef}
     >
@@ -59,8 +59,8 @@ export default function Message({ item, isMine }: Props) {
           <a
             href={decoratedHref}
             key={key}
-            target="_blank"
-            className="text-blue-600 hover:underline whitespace-pre-wrap"
+            target='_blank'
+            className='text-blue-600 hover:underline whitespace-pre-wrap'
             style={{
               overflowWrap: "break-word",
             }}
@@ -72,5 +72,5 @@ export default function Message({ item, isMine }: Props) {
         {item?.text}
       </Linkify>
     </div>
-  )
+  );
 }
