b71e654f70557fce7f14f30c5ba539e0957180b8
mahdirasti2 <mahdi.r@zemtrix.com>
2024-09-07 02:29:50 +0300

    Fix typing errors

--- src/components/shared/calendar-schedule/create-schedule-2/select-times/repeatable-events.tsx
+++
@@ -1,6 +0,0 @@
-import CotopiaSwitch from "@/components/shared-ui/c-switch";
-import React from "react";
-
-export default function RepeatableEvents() {
-  return <CotopiaSwitch label='Repeatable event?' />;
-}
