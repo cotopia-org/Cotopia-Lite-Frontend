048ee4024f98ac346871238270ba574554a29936
mahdirasti2 <mahdi.r@zemtrix.com>
2024-09-08 12:49:28 +0300

    sort chat item (direct) by nonce_id

--- src/components/shared/room/room-context/index.tsx
+++ src/components/shared/room/room-context/index.tsx
@@ -65,23 +65,24 @@ export default function RoomContext({
   const router = useRouter();
 
   const handleJoinRoom = async () => {
-    const res = await axiosInstance.get<FetchDataType<WorkspaceRoomJoinType>>(
-      `/rooms/${room_id}/join`
-    );
-
     //Join user to the room by socket request
-    if (socket) socket.emit("joinedRoom", room_id);
+    if (socket)
+      socket.emit("joinedRoom", room_id, () => {
+        axiosInstance
+          .get<FetchDataType<WorkspaceRoomJoinType>>(`/rooms/${room_id}/join`)
+          .then((res) => {
+            const livekitToken = res.data.data.token; //Getting livekit token from joinObject
 
-    const livekitToken = res.data.data.token; //Getting livekit token from joinObject
+            playSoundEffect("joined");
 
-    playSoundEffect("joined");
-
-    if (livekitToken) {
-      router.push(
-        `/workspaces/${workspace_id}/rooms/${room_id}?token=${livekitToken}`
-      );
-      return;
-    }
+            if (livekitToken) {
+              router.push(
+                `/workspaces/${workspace_id}/rooms/${room_id}?token=${livekitToken}`
+              );
+              return;
+            }
+          });
+      });
   };
 
   const [permissionState, setPermissionState] = useState({
--- src/components/shared/room/settings/chat/direct/directs/index.tsx
+++ src/components/shared/room/settings/chat/direct/directs/index.tsx
@@ -1,53 +1,60 @@
-import { UserMinimalType } from "@/types/user"
-import React from "react"
-import NotFound from "@/components/shared/layouts/not-found"
-import { useProfile } from "@/app/(pages)/(protected)/protected-wrapper"
-import UserCard from "../users/card"
-import { DirectType } from "@/types/direct"
+import { UserMinimalType } from "@/types/user";
+import React from "react";
+import NotFound from "@/components/shared/layouts/not-found";
+import { useProfile } from "@/app/(pages)/(protected)/protected-wrapper";
+import UserCard from "../users/card";
+import { DirectType } from "@/types/direct";
 
 type Props = {
-  search?: string
-  onSelect: (item: DirectType) => void
-  directs: DirectType[]
-}
+  search?: string;
+  onSelect: (item: DirectType) => void;
+  directs: DirectType[];
+};
 
 export default function Directs({ search, onSelect, directs }: Props) {
-  const { user } = useProfile()
+  const { user } = useProfile();
 
-  let finalDirects = [...directs]
+  let finalDirects = [...directs];
 
   if (search)
     finalDirects = finalDirects.filter((x) => {
       const allParticipantsUsername = x.participants
         .map((x) => x.username)
-        .join(" ")
-      const allParticipantsName = x.participants.map((x) => x.name).join(" ")
+        .join(" ");
+      const allParticipantsName = x.participants.map((x) => x.name).join(" ");
       return (
         new RegExp(search, "i").test(`/${allParticipantsUsername}/`) ||
         new RegExp(search, "i").test(`/${allParticipantsName}/`)
-      )
-    })
+      );
+    });
 
-  if (finalDirects.length === 0) return <NotFound title="No users found!" />
+  if (finalDirects.length === 0) return <NotFound title='No users found!' />;
 
   return (
-    <div className="flex flex-col gap-y-4">
-      {finalDirects.map((item, key) => {
-        let defaultMessage = item.last_message
-        return (
-          <UserCard
-            key={key}
-            user={
-              item.participants.find(
-                (x) => x.id !== user?.id
-              ) as UserMinimalType
-            }
-            defaultLatest={defaultMessage}
-            direct={item}
-            onClick={() => onSelect(item)}
-          />
+    <div className='flex flex-col gap-y-4'>
+      {finalDirects
+        .filter(
+          (x) =>
+            x?.last_message !== undefined && x?.last_message?.nonce_id !== null
         )
-      })}
+        //@ts-ignore
+        .sort((a, b) => b?.last_message?.nonce_id - a?.last_message?.nonce_id)
+        .map((item, key) => {
+          let defaultMessage = item.last_message;
+          return (
+            <UserCard
+              key={key}
+              user={
+                item.participants.find(
+                  (x) => x.id !== user?.id
+                ) as UserMinimalType
+              }
+              defaultLatest={defaultMessage}
+              direct={item}
+              onClick={() => onSelect(item)}
+            />
+          );
+        })}
     </div>
-  )
+  );
 }
