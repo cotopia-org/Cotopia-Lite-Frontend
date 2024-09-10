61ba9ff7c3251c5fadaf5c748d97a6bb97bf7ce7
mahdirasti2 <mahdi.r@zemtrix.com>
2024-09-10 13:21:08 +0300

    Fix and something important in update coordinates

--- src/components/shared/room/room-context/index.tsx
+++ src/components/shared/room/room-context/index.tsx
@@ -65,24 +65,41 @@ export default function RoomContext({
   const router = useRouter();
 
   const handleJoinRoom = async () => {
-    //Join user to the room by socket request
-    if (socket)
-      socket.emit("joinedRoom", room_id, () => {
-        axiosInstance
-          .get<FetchDataType<WorkspaceRoomJoinType>>(`/rooms/${room_id}/join`)
-          .then((res) => {
-            const livekitToken = res.data.data.token; //Getting livekit token from joinObject
+    axiosInstance
+      .get<FetchDataType<WorkspaceRoomJoinType>>(`/rooms/${room_id}/join`)
+      .then((res) => {
+        const livekitToken = res.data.data.token; //Getting livekit token from joinObject
 
-            playSoundEffect("joined");
+        if (socket) socket.emit("joinedRoom", room_id);
 
-            if (livekitToken) {
-              router.push(
-                `/workspaces/${workspace_id}/rooms/${room_id}?token=${livekitToken}`
-              );
-              return;
-            }
-          });
+        playSoundEffect("joined");
+
+        if (livekitToken) {
+          router.push(
+            `/workspaces/${workspace_id}/rooms/${room_id}?token=${livekitToken}`
+          );
+          return;
+        }
       });
+
+    //Join user to the room by socket request
+    // if (socket)
+    //   socket.emit("joinedRoom", room_id, () => {
+    //     axiosInstance
+    //       .get<FetchDataType<WorkspaceRoomJoinType>>(`/rooms/${room_id}/join`)
+    //       .then((res) => {
+    //         const livekitToken = res.data.data.token; //Getting livekit token from joinObject
+
+    //         playSoundEffect("joined");
+
+    //         if (livekitToken) {
+    //           router.push(
+    //             `/workspaces/${workspace_id}/rooms/${room_id}?token=${livekitToken}`
+    //           );
+    //           return;
+    //         }
+    //       });
+    //   });
   };
 
   const [permissionState, setPermissionState] = useState({
