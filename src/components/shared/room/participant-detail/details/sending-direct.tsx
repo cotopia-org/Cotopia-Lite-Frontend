a499497ca766497d5946ce11da3459d36b782175
mahdirasti2 <mahdi.r@zemtrix.com>
2024-09-22 16:58:21 +0300

    remove sending direct from participants details card

--- src/components/shared/room/participant-detail/details/sending-direct.tsx
+++ src/components/shared/room/participant-detail/details/sending-direct.tsx
@@ -7,7 +7,7 @@ export default function SendingDirect() {
   const { user } = useUserDetail();
 
   return (
-    <form className='flex flex-row items-center gap-x-2'>
+    <div className='flex flex-row items-center gap-x-2'>
       <CotopiaInput
         placeholder='Sending message ...'
         className='flex-1 !ring-offset-0 !outline-none !ring-0'
@@ -15,6 +15,6 @@ export default function SendingDirect() {
       <CotopiaIconButton className='text-black w-8 h-8'>
         <SendHorizonal size={16} />
       </CotopiaIconButton>
-    </form>
+    </div>
   );
 }
