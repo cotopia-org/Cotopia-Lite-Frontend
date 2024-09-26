3d326ff5dd6cfc7905ed6c547351f79db5750b73
Youssef9005 <youssefsamehdev@gmail.com>
2024-09-26 08:21:04 +0300

    Fix conflicts between 2-add-sketching-feature-room branch and dev

--- src/components/shared/room/tools/bottom-middle/index.tsx
+++ src/components/shared/room/tools/bottom-middle/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AddButtonTool from "./add-button";
 import BroadcastButtonTool from "./broadcast-button";
 import ChatButtonTool from "./chat-button";
@@ -8,37 +7,19 @@ import VideoButtonTool from "./video-button";
 import VoiceButtonTool from "./voice-button";
 import MegaPhoneButtonTool from "./megaphone-button";
 import GridViewButtonTool from "./gridview-button";
-import { useRoomSpatialContext } from "@/context/room-spatial-context";
-=======
-import AddButtonTool from "./add-button"
-import BroadcastButtonTool from "./broadcast-button"
-import ChatButtonTool from "./chat-button"
-import EditButtonTool from "./edit-button"
-import ShareScreenButtonTool from "./share-screen-button"
-import VideoButtonTool from "./video-button"
-import VoiceButtonTool from "./voice-button"
-// import MegaPhoneButtonTool from "./megaphonew-button";
->>>>>>> 8969a45dadcbd794b8088d6aad7c6a5d84899632
 
 export default function BottomMiddleTools() {
   return (
     <div className="flex flex-row items-center bg-white rounded-xl p-2">
       <AddButtonTool />
-      <EditButtonTool/>
+      <EditButtonTool />
       {/* <ShareScreenButtonTool /> */}
       <ChatButtonTool />
       <BroadcastButtonTool />
-<<<<<<< HEAD
       {/* <VideoButtonTool /> */}
       {/* <VoiceButtonTool /> */}
-      <MegaPhoneButtonTool />
-      <GridViewButtonTool />
-=======
-      <VideoButtonTool />
-      <VoiceButtonTool />
       {/* <MegaPhoneButtonTool /> */}
       {/* <GridViewButtonTool /> */}
->>>>>>> 8969a45dadcbd794b8088d6aad7c6a5d84899632
     </div>
-  )
+  );
 }
--- src/hooks/use-window-size.ts
+++ src/hooks/use-window-size.ts
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 
-<<<<<<< HEAD
 const useWindowSize = (elementId?: string) => {
   const [windowSize, setWindowSize] = useState<{
     windowWidth: number;
@@ -29,42 +28,12 @@ const useWindowSize = (elementId?: string) => {
 
     // Add resize listener
     window.addEventListener("resize", updateSize);
-=======
-const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState<{
-    windowWidth: number;
-    windowHeight: number;
-  }>({ windowWidth: 0, windowHeight: 0 });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        windowWidth: window.innerWidth,
-        windowHeight: window.innerHeight,
-      });
-    };
-
-    // Set the initial width when loading the component
-    setWindowSize({
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight,
-    });
-
-    // Add a listener to resize the window
-    window.addEventListener("resize", handleResize);
->>>>>>> 8969a45dadcbd794b8088d6aad7c6a5d84899632
 
     // Cleanup listener on component unmount
     return () => {
-<<<<<<< HEAD
       window.removeEventListener("resize", updateSize);
     };
   }, [elementId]); // Ensure updates based on elementId
-=======
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
->>>>>>> 8969a45dadcbd794b8088d6aad7c6a5d84899632
 
   return { windowSize };
 };
